ab765e50b27ff5045b05cc5536505974af799001
Diego Silva <diego.silval@gmail.com>
2025-03-18 13:15:11 -0500

    Fix typos in artifactId for dependencies and add hibernate dialect mappings

--- src/main/java/com/apuntesdejava/jakartacoffeebuilder/mojo/persistence/AddEntitiesMojo.java
+++ src/main/java/com/apuntesdejava/jakartacoffeebuilder/mojo/persistence/AddEntitiesMojo.java
@@ -15,18 +15,13 @@
  */
 package com.apuntesdejava.jakartacoffeebuilder.mojo.persistence;
 
-import com.apuntesdejava.jakartacoffeebuilder.helper.JakartaEeHelper;
 import com.apuntesdejava.jakartacoffeebuilder.helper.JakartaPersistenceHelper;
-import com.apuntesdejava.jakartacoffeebuilder.helper.MavenProjectHelper;
-import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
-import org.apache.maven.project.ProjectBuilder;
 
 import java.io.File;
 
@@ -47,22 +42,10 @@ public class AddEntitiesMojo extends AbstractMojo {
     @Parameter(defaultValue = "${project}", readonly = true)
     private MavenProject mavenProject;
 
-    @Component
-    private ProjectBuilder projectBuilder;
-
-    @Parameter(
-        defaultValue = "${session}",
-        readonly = true,
-        required = true
-    )
-    private MavenSession mavenSession;
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         try {
             var log = getLog();
-            var fullProject = MavenProjectHelper.getInstance()
-                                                .getFullProject(mavenSession, projectBuilder, mavenProject);
-            JakartaEeHelper.getInstance().checkDataDependencies(fullProject, log);
             log.info("Adding entities from file: " + entitiesFile.getAbsolutePath());
             if (!entitiesFile.exists()) {
                 log.error("Entities file not found: " + entitiesFile.getAbsolutePath());
